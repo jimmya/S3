a066a8a1271109170f1f6e4723cb5d8188d8eb10
Jimmy Arts <jimmy.arts@ahold.com>
2020-03-28 15:38:28 +0100

    re-add S3Kit

--- Package.swift
+++ Package.swift
@@ -8,13 +8,19 @@ let package = Package(
     ],
     products: [
         .library(name: "S3Signer", targets: ["S3Signer"]),
+        .library(name: "S3Kit", targets: ["S3Kit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.1"),
+        .package(url: "https://github.com/LiveUI/XMLCoding.git", from: "0.1.0"),
     ],
     targets: [
         .target(name: "S3Signer", dependencies: [
             .product(name: "Vapor", package: "vapor"),
+        ]),
+        .target(name: "S3Kit", dependencies: [
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "XMLCoding", package: "XMLCoding"),
         ]),
     ]
 )
